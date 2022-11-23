7f6134c9081eb537edaa256eacf2711c5d06434c
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2022-11-23 19:41:50 +0530

    Adds second widget

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import styles from './App.module.css';
 import LinearSlider from './components/linearSlider/linearSlider';
 import CircularProgress from './components/circularProgress/circularProgress';
 import Widget1 from './components/widget1/widget1';
+import Widget2 from './components/widget2/widget2';
 
 function App() {
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", gap: "20px" }}>
       <div className={styles.widgetContainer}>
         <Widget1 />
+      </div>
+
+      <div className={styles.widgetContainer}>
+        <Widget2 />
       </div>
     </div>
   );
