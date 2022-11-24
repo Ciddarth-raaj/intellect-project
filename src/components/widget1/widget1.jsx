63c3baaa2ddda42a8fb566637d2b62b6db4e36a5
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2022-11-24 07:35:14 +0530

    Adds new comment

--- src/components/widget1/widget1.jsx
+++ src/components/widget1/widget1.jsx
@@ -5,6 +5,7 @@ import LinearSlider from "../linearSlider/linearSlider";
 // Props
 // size - used to set the width and height of the widget
 // max - used to set the max value in linear slider
+// min - used to set the min value in linear slider
 
 const Widget1 = ({ size = 200, max = 10, min = 0 }) => {
   const [value, setValue] = useState(10);
